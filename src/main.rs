9e50eef31d89db9eb5b9d0abd0804b6075d69461
SalsaGal <fielddashos@gmail.com>
2022-07-17 21:42:40 +1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    source: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    if let Ok(contents) = fs::read_to_string(args.source) {
+    }
+}
