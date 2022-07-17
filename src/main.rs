8c136c40ab441b8c1614979fd90336a3663060ce
SalsaGal <fielddashos@gmail.com>
2022-07-17 23:18:36 +1000

    Add input flags

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,26 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
+    /// Input files
     source: String,
+    /// Print out tokens when they're lexed
+    #[clap(short, long)]
+    tokens: bool,
+    /// Print out instructions when they're parsed
+    #[clap(short, long)]
+    instructions: bool,
 }
 
 fn main() {
     let args = Args::parse();
     if let Ok(contents) = fs::read_to_string(args.source) {
         let tokens = lexer::lex(contents);
-        dbg!(&tokens);
+        if args.tokens {
+            dbg!(&tokens);
+        }
         let instructions = parser::parse(tokens);
-        dbg!(&instructions);
+        if args.instructions {
+            dbg!(&instructions);
+        }
     }
 }
